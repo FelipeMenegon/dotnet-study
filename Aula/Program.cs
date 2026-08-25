b1c29c6d2781d7bf91164edea04ea067837655cc
FelipeM <126184555+FelipeMenegon@users.noreply.github.com>
2026-08-25 11:24:12 -0300

    Aula02

---
+++ Aula/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Aula01;
+
+public class Program
+{
+    public static void Main()
+    {
+        string nome = "João";
+        int idade = 25;
+        double altura = 1.75;
+        char genero = 'M';
+    }
+    
+}

4f3e76b2b61acfbdc0fe8a022e2a353d917eaaf2
FelipeM <126184555+FelipeMenegon@users.noreply.github.com>
2026-08-31 09:18:05 -0300

    Aula 9 - For

--- Aula/Program.cs
+++ Aula/Program.cs
@@ -4,12 +4,9 @@ public class Program
 {
     public static void Main()
     {
-        int i = 0;
-
-        while (i <= 5)
+        for (int i = 1; i <= 10; i++)
         {
             Console.WriteLine(i);
-            i++;
         }
     }
 }

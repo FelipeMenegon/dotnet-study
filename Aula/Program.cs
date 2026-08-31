de27214c76e75c647ed1f5af6ddd06f79bdd0e96
FelipeM <126184555+FelipeMenegon@users.noreply.github.com>
2026-08-31 08:38:39 -0300

    Aula03.01 - Constantes

--- Aula/Program.cs
+++ Aula/Program.cs
@@ -4,10 +4,21 @@ public class Program
 {
     public static void Main()
     {
-        string nome = "João";
-        int idade = 25;
-        double altura = 1.75;
-        char genero = 'M';
+        int number = 10; // Declaração e inicialização da variável 'number' com o valor 10
+
+        number = 20; // Atribuição de um novo valor à variável 'number'
+
+        Console.WriteLine("O valor da variável 'number' é: " + number);
+
+        const int constantNumber = 1000;
+        Console.WriteLine("O valor da variável 'constantNumber' é: " + constantNumber);
+
+        var variable = "Hello, World!"; // Declaração de uma variável usando 'var' - Tipo String
+        variable = 10; // Atribuição com erro de tipo - Não é permitido atribuir um valor inteiro a uma variável que foi inferida como string
+    
+        dynamic variables = "Hello, World!"; // Declaração de uma variável usando 'dynamic' - Tipo String   
+
+        dynamic variables = 10; // Atribuição de um valor inteiro à variável 'variables' - Permitido com 'dynamic'
     }
     
 }
